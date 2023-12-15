3244c9c3727f5a7a9da08cff99f99d9e71b5afab
Mustafaursn1 <69422949+Mustafaursn1@users.noreply.github.com>
2023-12-15 21:08:23 +0100

    add cucumber AmazonSearch

---
+++ src/test/java/pages/AmazonPages.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class AmazonPages {
+    public AmazonPages() { //constructor olusturup public yaptik
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id="twotabsearchtextbox")
+    public WebElement searchBox;
+    @FindBy(xpath = "//div[@class='a-section a-spacing-small a-spacing-top-small']")
+    public WebElement writingOfResult;
+}
